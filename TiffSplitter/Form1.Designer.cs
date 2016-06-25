6d0ec0d641ebe8f28dda85a07254546b16043ed5
Nate Marler <nmarler@sawgrassconsulting.com>
2016-06-24 20:36:50 -0500

    Initial Commit

---
+++ TiffSplitter/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace TiffSplitter
+{
+    internal partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(21, 34);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(294, 20);
+            this.txtFolder.TabIndex = 0;
+            this.txtFolder.TextChanged += new System.EventHandler(this.txtFolder_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Folder";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(321, 27);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(83, 33);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(144, 75);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(121, 43);
+            this.btnSplit.TabIndex = 3;
+            this.btnSplit.Text = "Split";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(265, 39);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "INSTRUCTIONS\r\n1. Specify the folder to analyze (subfolders are ignored)\r\n2. Click" +
+    " the \"Split\" button";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(365, 52);
+            this.label4.TabIndex = 6;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 256);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFolder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TIFF Splitter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ TiffSplitter/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TiffSplitter
+{
+    internal partial class Form1 : Form
+    {
+
+        internal Form1()
+        {
+            InitializeComponent();
+
+            // error check
+            if( null == txtFolder )
+                return;
+
+            // fill in path to this executable
+            txtFolder.Text = Path.GetDirectoryName( Application.ExecutablePath );
+
+            // move carat to end of text box
+            txtFolder.Select(txtFolder.TextLength,0);
+        }
+
+
+        private void btnBrowse_Click(
+            object sender,
+            System.EventArgs e )
+        {
+            // error check
+            if( null == txtFolder )
+                return;
+
+            // create folder browser dialog
+            FolderBrowserDialog lobjDialog = new FolderBrowserDialog
+            {
+                SelectedPath = txtFolder.Text
+            };
+
+            // user interaction
+            DialogResult dlgResult = lobjDialog.ShowDialog();
+            if( dlgResult == DialogResult.OK )
+            {
+                // Get the path for the selected folder
+                txtFolder.Text = lobjDialog.SelectedPath;
+            }
+        }
+
+
+        private void txtFolder_TextChanged( 
+            object sender, 
+            System.EventArgs e )
+        {
+            if( null != btnSplit )
+                btnSplit.Enabled = !string.IsNullOrEmpty( txtFolder?.Text );
+        }
+
+
+        private void btnSplit_Click( 
+            object sender, 
+            System.EventArgs e )
+        {
+            // error check
+            if( string.IsNullOrEmpty( txtFolder?.Text ) ) {
+                DisplayError( "Must specify folder to analyze" );
+                return;
+            }
+
+            // error check
+            if( !Directory.Exists( txtFolder.Text ) ) {
+                DisplayError( "Must specify a folder that already exists" );
+                return;
+            }
+
+            // process files in the directory
+            int splitCount = Directory.EnumerateFiles( txtFolder.Text ).Count( Split );
+
+            // results to user
+            if( splitCount == 0 )
+                MessageBox.Show( "No files were split", "Result", MessageBoxButtons.OK, MessageBoxIcon.Warning );
+            else
+                MessageBox.Show( $"{splitCount} file{( splitCount == 1 ? " was" : "s were" )} split", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information );
+        }
+
+
+        private static bool Split(
+            string file )
+        {
+            // verify parameter
+            if( string.IsNullOrEmpty(file))
+                return false;
+
+            // verify parameter represents a file
+            if( !File.Exists( file ) )
+                return false;
+
+            // verify parameter is a tiff file (well, at least that it has proper extension)
+            if( !file.ToLower().EndsWith( ".tif" ) && !file.ToLower().EndsWith( ".tiff" ) )
+                return false;
+
+            // create an image object to work with
+            using( Image tiffImage = Image.FromFile( file ) )
+            {
+                // get number of pages
+                int numPages =
+                    tiffImage.GetFrameCount( new FrameDimension( tiffImage.FrameDimensionsList[0] ) );
+
+                // is there anything to Split?
+                if( 2 > numPages )
+                    return false;
+
+                // get tiff codec info
+                ImageCodecInfo tiffCodecInfo =
+                    ImageCodecInfo.GetImageEncoders().FirstOrDefault( t => t?.MimeType == "image/tiff" );
+
+                // error check
+                if( null == tiffCodecInfo ) {
+                    DisplayError( "Internal Error - could not obtain TIFF codec" );
+                    return false;
+                }
+
+                // time to Split
+                foreach( var guid in tiffImage.FrameDimensionsList ) 
+                {
+                    for( int index = 0; index < numPages; index++ )
+                    {
+                        FrameDimension currentFrame = new FrameDimension( guid );
+                        tiffImage.SelectActiveFrame( currentFrame, index );
+                        tiffImage.Save( MakeIndexedFileName(file,index), tiffCodecInfo, null );
+                    }
+                }
+            }
+
+            // move the original file to subfolder
+            File.Move(file, MakeSubfolderFileName(file) );
+
+            // success
+            return true;
+        }
+
+
+        private static void DisplayError(
+            string errMessage )
+        {
+            MessageBox.Show( errMessage, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error );
+        }
+
+
+        private static string GetDateTimeStamp()
+        {
+            return DateTime.Now.ToString( "yyyyMMdd-HHmmss.fff" );
+        }
+
+
+        /// <summary>
+        /// Insert index into the provided filename (insert just prior to the file extension). Guaranteed
+        /// to be unique.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private static string MakeIndexedFileName(
+            string fileName,
+            int index )
+        {
+            string newPath;
+
+            bool retryFlag = false;
+            int retryCount = 1;
+
+            // make unique
+            while( true )
+            {
+                // generate new name (add counter if we are retry-ing)
+                string newName =
+                    !retryFlag
+                    ? $"{Path.GetFileNameWithoutExtension( fileName )}.{index}{Path.GetExtension( fileName )}"
+                    : $"{Path.GetFileNameWithoutExtension( fileName )}.{index} ({retryCount++}){Path.GetExtension( fileName )}";
+
+                newPath =
+                    Path.Combine( Path.GetDirectoryName( fileName ), newName );
+
+                // unique?
+                if( !File.Exists( newPath ) )
+                    break;
+
+                // try again
+                retryFlag = true;
+            }
+
+            // done
+            return newPath;
+        }
+
+
+        /// <summary>
+        /// Insert subfolder named "Originals" just prior to the filename. Create the subfolder
+        /// if it doesn't already exist. File name guaranteed to be unique.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        private static string MakeSubfolderFileName(
+            string fileName )
+        {
+            // create new path by inserting subfolder
+            string newPath = Path.Combine( Path.GetDirectoryName( fileName ), "Multi-Image TIFF Originals" );
+
+            // create if doesn't exist
+            if( !Directory.Exists( newPath ) )
+                Directory.CreateDirectory( newPath );
+
+            // add the filename back on
+            string newName = Path.Combine( newPath, Path.GetFileName( fileName ) );
+
+            // verify unique, if not rename file
+            int retryCount = 1;
+            while( true )
+            {
+                // unique?
+                if( !File.Exists( newName ) )
+                    break;
+
+                // add counter and try again
+                string s =
+                    $"{Path.GetFileNameWithoutExtension( fileName )} ({retryCount++}){Path.GetExtension( fileName )}";
+                newName = Path.Combine( newPath, s );
+            }
+
+            // done
+            return newName;
+        }
+    }
+}
